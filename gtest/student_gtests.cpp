fd1a2dc9ab08a872afc7267e08c01f33df55b707
Kaixiang Zhuang <zhuangk2@circinus-29.ics.uci.edu>
2023-05-10 19:30:08 -0700

    Submission commit of HW5.

--- gtest/student_gtests.cpp
+++ gtest/student_gtests.cpp
@@ -162,7 +162,7 @@ TEST(ListTests, Index) {
     Node* const l1 = list::from_string("foo");
     Node* const l2 = list::from_string("a");
     
-    EXPECT_EQ(list::index(l1, l1->next), 1);
+    EXPECT_EQ(list::index(l1, list::last(l1)), length(l1)-1);
     EXPECT_EQ(list::index(l2, l1), -1);
 
     list::free(l1);
